394df368c4dfb333019f92e28ed9696ddddf7ac6
FernandoVarela25 <106282950+FernandoVarela25@users.noreply.github.com>
2023-11-26 21:50:57 -0600

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+// service-worker.js
+
+const CACHE_NAME = 'mi-cache';
+const urlsToCache = [
+  '/',
+  '/portada.html',
+  '/utps.cs',
+  '/script.js'
+  // Agrega aquí los archivos que quieres que se almacenen en caché
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Caché abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = ['mi-cache'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
